b597208d29438bff9cdadf66a9d21b4cd7a280d2
ranadeep <ranadeepreddy777@gmail.com>
2023-10-15 18:21:25 -0400

    added accept request functionality

--- src/app/components/friends/friends.component.ts
+++ src/app/components/friends/friends.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { IUserDetails } from 'src/app/models/user.model';
+import { AdminService } from 'src/app/services/admin.service';
+
 
 @Component({
   selector: 'app-friends',
@@ -6,5 +10,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent {
+public userRecords: IUserDetails[];
+userdata:any
+constructor(public adminService:AdminService){
+  this.userRecords=[]
+}
+public ngOnInit(): void {
+  this.loadUsers();
+}
+loadUsers(){
+  this.adminService.getUsers().subscribe((response: any) => {
+    this.userdata = localStorage.getItem('currentUser')
+    var id = JSON.parse(this.userdata).id
+    for(var i=0;i< response.length;i++){
+      var jsonobj:any={}
+      var keys=Object.keys(response[i])
+      if(keys.includes("Requests")){
+      for(var j=0;j<response[i]["Requests"].length;j++){
+      if(response[i]["Requests"][j]["status"]=="pending" && response[i]["Requests"][j]["friend"]!=id){
+        jsonobj = response[i];
+      }
+    }
+    }
+      this.userRecords.push(jsonobj)
 
+  }
+  this.userdata = localStorage.getItem('currentUser')
+  var id = JSON.parse(this.userdata).id
+  this.userRecords= this.userRecords.filter(item=>item["id"]!=id)
+  this.userRecords= this.userRecords.filter(item=>Object.keys(item).length>0)
+  });
+}
+
+acceptrequest(id:any){
+  this.adminService.connectfriends({"Requests":[{"status":"accepted","friend":JSON.parse(this.userdata).id}]},id).subscribe((respondr:any)=>{
+    alert('Frind request sent')
+  })
+  this.loadUsers()
+
+}
 }
--- src/app/components/network/network.component.ts
+++ src/app/components/network/network.component.ts
@@ -10,36 +10,64 @@ import { AdminService } from 'src/app/services/admin.service';
 })
 export class NetworkComponent {
   public userRecords: IUserDetails[];
+  public userdata1:IUserDetails[]
+  public tempdata:any
   userdata:any
   constructor(public adminService:AdminService){
     this.userRecords=[]
     this.userdata=[]
+    this.userdata1=[]
+    this.tempdata=[]
   }
   public ngOnInit(): void {
     this.loadUsers();
   }
 
   loadUsers(){
+    this.userdata = localStorage.getItem('currentUser')
+    var id = JSON.parse(this.userdata).id
     this.adminService.getUsers().subscribe((response: any) => {
-      for(var i=0;i< response.length;i++){
+      for(var i=0;i<response.length;i++){
+        var jsonobj:any={}
         var keys=Object.keys(response[i])
-        var temp = keys.includes("Requests")
         if(!keys.includes("Requests")){
-          this.userRecords = response;
-          this.userdata = localStorage.getItem('currentUser')
-          var id = JSON.parse(this.userdata).id
-          this.userRecords= this.userRecords.filter(item=>item["id"]!=id)
+          jsonobj = response[i];
+        }
+        if(keys.includes("Requests")){
+          for(var j=0;j<response[i]["Requests"].length;j++){
+               if(response[i]["Requests"][j]["status"]=="pending" && response[i]["Requests"][j]["friend"]!=id){
+                jsonobj=response[i]
+               }
+          }
         }
         else{
-          this.userRecords=[]
+          this.userdata1.push(response[i])
         }
+        this.userRecords.push(jsonobj)
+
     }
+    this.userRecords= this.userRecords.filter(item=>item["id"]!=id)
+    this.userRecords= this.userRecords.filter(item=>Object.keys(item).length>0)
+
     });
   }
   sendrequest(id:any){
-    this.adminService.connectfriends({"Requests":{"status":"pending","friend":JSON.parse(this.userdata).id}},id).subscribe((respondr:any)=>{
-
+    var requestsdata:any;
+    requestsdata = this.userdata1.filter((item: { [x: string]: any; })=>item["id"]==id)
+    if(Object.keys(requestsdata).includes("Requests")){
+     for(var i=0;i< requestsdata["Requests"].length;i++){
+       this.tempdata.push(requestsdata["Requests"][i])
+     }
+     this.tempdata.push({"status":"pending","friend":JSON.parse(this.userdata).id})
+    }
+    else{
+      this.tempdata =[{"status":"pending","friend":JSON.parse(this.userdata).id}]
+    }
+    
+    this.adminService.connectfriends({"Requests":this.tempdata},id).subscribe((respondr:any)=>{
+      alert('Frind request sent')
     })
+    this.userRecords=[]
     this.loadUsers()
   }
 
